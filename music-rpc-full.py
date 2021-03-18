06dccb418eba6fa2f96972cb02294957db3705c0
NextFire <git@yuru.moe>
2021-03-18 23:40:35 +0100

    code refactoring

--- music-rpc-full.py
+++ music-rpc-full.py
@@ -1,94 +1,85 @@
 #!/usr/bin/env python3
 from pypresence import Presence
-import time, os, subprocess
+import subprocess
+import time
+
 
 # Initialize RPC
-CLIENT_ID = "773825528921849856" # DO NOT CHANGE
-RPC = Presence(CLIENT_ID) # Initialize the Presence client
+CLIENT_ID = '773825528921849856'  # DO NOT CHANGE
+RPC = Presence(CLIENT_ID)  # Initialize the Presence client
 
 # Constants
-APPICON = "appicon" # DO NOT CHANGE
-APPICON_DESC = "Apple Music for macOS"
+APPICON = 'appicon'  # DO NOT CHANGE
+APPICON_DESC = 'Apple Music for macOS'
+
 
 # Return number of Apple Music process running (int)
 def music_sessions_as():
     return int(subprocess.run(["osascript",
-                "-e", "tell application \"System Events\"",
-                "-e", "count (every process whose name is \"Music\")",
-                "-e", "end tell"], capture_output=True).stdout.decode('utf-8'))
+                               "-e", "tell application \"System Events\"",
+                               "-e", "count (every process whose name is \"Music\")",
+                               "-e", "end tell"], capture_output=True, text=True).stdout)
 
-# Return if Apple Music is "playing", "paused" or "stopped"
+
+# Return if Apple Music is 'playing', 'paused' or 'stopped'
 # Only works when Apple Music is currently running
 def music_state_as():
     return subprocess.run(["osascript",
-            "-e", "tell application \"Music\"",
-            "-e", "if player state is playing then",
-            "-e", "set playerStateText to \"playing\"",
-            "-e", "else if player state is paused then",
-            "-e", "set playerStateText to \"paused\"",
-            "-e", "else",
-            "-e", "set playerStateText to \"stopped\"",
-            "-e", "end if",
-            "-e", "end tell"], capture_output=True).stdout.decode('utf-8').rstrip()
+                           "-e", "tell application \"Music\"",
+                           "-e", "if player state is playing then",
+                           "-e", "set playerStateText to \"playing\"",
+                           "-e", "else if player state is paused then",
+                           "-e", "set playerStateText to \"paused\"",
+                           "-e", "else",
+                           "-e", "set playerStateText to \"stopped\"",
+                           "-e", "end if",
+                           "-e", "end tell"], capture_output=True, text=True).stdout.rstrip()
+
 
 # Return [name, artist, album, year, duration, player position] of current track
 # Only works when Apple Music is currently running
 def music_info_as():
     return subprocess.run(["osascript",
-            "-e", "tell application \"Music\"",
-            "-e", "get {name, artist, album, year, duration} of current track & {player position}",
-            "-e", "end tell"], capture_output=True).stdout.decode('utf-8').rstrip().split(", ")
+                           "-e", "tell application \"Music\"",
+                           "-e", "get {name, artist, album, year, duration} of current track & {player position}",
+                           "-e", "end tell"], capture_output=True, text=True).stdout.rstrip().split(', ')
 
-# Return "playing","paused" or "stopped" at each call whenever Apple Music is running or not
+
+# Return 'playing', 'paused' or 'stopped' at each call whenever Apple Music is running or not
 def music_state():
-    if music_sessions_as() > 0: return music_state_as()
-    else: return "stopped"
+    return music_state_as() if music_sessions_as() else 'stopped'
 
-RPC.connect() # Start the handshake loop
-while True: # The presence will stay on as long as the program is running
+
+# Start the handshake loop
+RPC.connect()
+# The presence will stay on as long as the program is running
+while True:
     state = music_state()
-    if state == "playing":
+
+    if state in ('playing', 'paused'):
         infos = music_info_as()
         try:
             RPC.update(
-            large_image = APPICON,
-            large_text = APPICON_DESC,
-            small_image = "playing",
-            small_text = "Playing『" + infos[0] + "』by " + infos[1] + " (" + infos[3] + ")",
-            details = infos[0],
-            state = infos[1] + " — " + infos[2] + " (" + infos[3] + ")",
-            end = time.time() + float(infos[4]) - float(infos[5]))
-        except:
-            RPC.update(
-            large_image = APPICON,
-            large_text = APPICON_DESC,
-            small_image = "playing",
-            small_text = "Playing",
-            details = "Apple Music is playing",
-            state = "Details unavailable")
-    elif state == "paused":
-        infos = music_info_as()
-        try:
-            RPC.update(
-            large_image = APPICON,
-            large_text = APPICON_DESC,
-            small_image = "paused",
-            small_text = "Paused『" + infos[0] + "』by " + infos[1] + " (" + infos[3] + ")",
-            details = infos[0],
-            state = infos[1] + " — " + infos[2] + " (" + infos[3] + ")")
-        except:
-            RPC.update(
-            large_image = APPICON,
-            large_text = APPICON_DESC,
-            small_image = "paused",
-            small_text = "Paused",
-            details = "Apple Music is paused",
-            state = "Details unavailable")
+                large_image=APPICON,
+                large_text=APPICON_DESC,
+                small_image=state,
+                small_text=f'{state.capitalize()}『{infos[0]}』by {infos[1]} ({infos[3]})',
+                details=infos[0],
+                state=f'{infos[1]} — {infos[2]} ({infos[3]})',
+                end=time.time() + float(infos[4]) - float(infos[5]) if state == 'playing' else None)
+        except IndexError:
+            RPC.update(large_image=APPICON,
+                       large_text=APPICON_DESC,
+                       small_image=state,
+                       small_text=state.capitalize(),
+                       details=f'Apple Music is {state}',
+                       state='Details unavailable')
+
     else:
-        RPC.update(
-        large_image = APPICON,
-        large_text = APPICON_DESC,
-        small_image = "stopped",
-        small_text = "Stopped",
-        details = "Apple Music is stopped")
+        RPC.update(large_image=APPICON,
+                   large_text=APPICON_DESC,
+                   small_image='stopped',
+                   small_text='Stopped',
+                   details='Apple Music is stopped')
+
     time.sleep(15)
--- music-rpc-mini.py
+++ music-rpc-mini.py
@@ -1,72 +1,86 @@
 #!/usr/bin/env python3
 from pypresence import Presence
-import time, os, subprocess
+import subprocess
+import time
+
 
 # Initialize RPC
-CLIENT_ID = "773825528921849856" # DO NOT CHANGE
-RPC = Presence(CLIENT_ID) # Initialize the Presence client
+CLIENT_ID = '773825528921849856'  # DO NOT CHANGE
+RPC = Presence(CLIENT_ID)  # Initialize the Presence client
 
 # Constants
-APPICON = "appicon" # DO NOT CHANGE
-APPICON_DESC = "Apple Music for macOS"
+APPICON = 'appicon'  # DO NOT CHANGE
+APPICON_DESC = 'Apple Music for macOS'
+
 
 # Return number of Apple Music process running (int)
 def music_sessions_as():
     return int(subprocess.run(["osascript",
-                "-e", "tell application \"System Events\"",
-                "-e", "count (every process whose name is \"Music\")",
-                "-e", "end tell"], capture_output=True).stdout.decode('utf-8'))
+                               "-e", "tell application \"System Events\"",
+                               "-e", "count (every process whose name is \"Music\")",
+                               "-e", "end tell"], capture_output=True, text=True).stdout)
 
-# Return if Apple Music is "playing" or "stopped"
+
+# Return if Apple Music is 'playing', 'paused' or 'stopped'
 # Only works when Apple Music is currently running
 def music_state_as():
     return subprocess.run(["osascript",
-            "-e", "tell application \"Music\"",
-            "-e", "if player state is playing then",
-            "-e", "set playerStateText to \"playing\"",
-            "-e", "else",
-            "-e", "set playerStateText to \"stopped\"",
-            "-e", "end if",
-            "-e", "end tell"], capture_output=True).stdout.decode('utf-8').rstrip()
+                           "-e", "tell application \"Music\"",
+                           "-e", "if player state is playing then",
+                           "-e", "set playerStateText to \"playing\"",
+                           "-e", "else if player state is paused then",
+                           "-e", "set playerStateText to \"paused\"",
+                           "-e", "else",
+                           "-e", "set playerStateText to \"stopped\"",
+                           "-e", "end if",
+                           "-e", "end tell"], capture_output=True, text=True).stdout.rstrip()
+
 
 # Return [name, artist, album, year, duration, player position] of current track
 # Only works when Apple Music is currently running
 def music_info_as():
     return subprocess.run(["osascript",
-            "-e", "tell application \"Music\"",
-            "-e", "get {name, artist, album, year, duration} of current track & {player position}",
-            "-e", "end tell"], capture_output=True).stdout.decode('utf-8').rstrip().split(", ")
+                           "-e", "tell application \"Music\"",
+                           "-e", "get {name, artist, album, year, duration} of current track & {player position}",
+                           "-e", "end tell"], capture_output=True, text=True).stdout.rstrip().split(', ')
 
-# Return "playing" or "stopped" at each call whenever Apple Music is running or not
+
+# Return 'playing', 'paused' or 'stopped' at each call whenever Apple Music is running or not
 def music_state():
-    if music_sessions_as() > 0: return music_state_as()
-    else: return "stopped"
+    return music_state_as() if music_sessions_as() else 'stopped'
 
-while True: # The presence will stay on as long as the program is running
+
+# The presence will stay on as long as the program is running
+while True:
+    state = music_state()
+
     # If music is playing, connect to RPC
-    if music_state() == "playing":
+    if state == 'playing':
         RPC.connect()
+
         # Update card while still playing music
-        while music_state() == "playing":
+        while state == 'playing':
             infos = music_info_as()
             try:
                 RPC.update(
-                large_image = APPICON,
-                large_text = APPICON_DESC,
-                small_image = "playing",
-                small_text = "Playing『" + infos[0] + "』by " + infos[1] + " (" + infos[3] + ")",
-                details = infos[0],
-                state = infos[1] + " — " + infos[2] + " (" + infos[3] + ")",
-                end = time.time() + float(infos[4]) - float(infos[5]))
-            except:
-                RPC.update(
-                large_image = APPICON,
-                large_text = APPICON_DESC,
-                small_image = "playing",
-                small_text = "Playing",
-                details = "Apple Music is playing",
-                state = "Details unavailable")
+                    large_image=APPICON,
+                    large_text=APPICON_DESC,
+                    small_image=state,
+                    small_text=f'{state.capitalize()}『{infos[0]}』by {infos[1]} ({infos[3]})',
+                    details=infos[0],
+                    state=f'{infos[1]} — {infos[2]} ({infos[3]})',
+                    end=time.time() + float(infos[4]) - float(infos[5]))
+            except IndexError:
+                RPC.update(large_image=APPICON,
+                           large_text=APPICON_DESC,
+                           small_image=state,
+                           small_text=state.capitalize(),
+                           details=f'Apple Music is {state}',
+                           state='Details unavailable')
             time.sleep(15)
+            state = music_state()
+
         # Music stopped, close connection
         RPC.close()
+
     time.sleep(15)
