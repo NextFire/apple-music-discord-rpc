86623548a70485b3cf72398914610ef8322f6080
NextFire <git@yuru.moe>
2021-06-09 09:10:01 +0200

    hide year if unknown, black formatting

--- music-rpc-mini.py
+++ music-rpc-mini.py
@@ -15,34 +15,73 @@ APPICON_DESC = 'Apple Music for macOS'
 
 # Return number of Apple Music process running (int)
 def music_sessions_as():
-    return int(subprocess.run(["osascript",
-                               "-e", "tell application \"System Events\"",
-                               "-e", "count (every process whose name is \"Music\")",
-                               "-e", "end tell"], capture_output=True, text=True).stdout)
+    return int(
+        subprocess.run(
+            [
+                "osascript",
+                "-e",
+                "tell application \"System Events\"",
+                "-e",
+                "count (every process whose name is \"Music\")",
+                "-e",
+                "end tell",
+            ],
+            capture_output=True,
+            text=True,
+        ).stdout
+    )
 
 
 # Return if Apple Music is 'playing', 'paused' or 'stopped'
 # Only works when Apple Music is currently running
 def music_state_as():
-    return subprocess.run(["osascript",
-                           "-e", "tell application \"Music\"",
-                           "-e", "if player state is playing then",
-                           "-e", "set playerStateText to \"playing\"",
-                           "-e", "else if player state is paused then",
-                           "-e", "set playerStateText to \"paused\"",
-                           "-e", "else",
-                           "-e", "set playerStateText to \"stopped\"",
-                           "-e", "end if",
-                           "-e", "end tell"], capture_output=True, text=True).stdout.rstrip()
+    return subprocess.run(
+        [
+            "osascript",
+            "-e",
+            "tell application \"Music\"",
+            "-e",
+            "if player state is playing then",
+            "-e",
+            "set playerStateText to \"playing\"",
+            "-e",
+            "else if player state is paused then",
+            "-e",
+            "set playerStateText to \"paused\"",
+            "-e",
+            "else",
+            "-e",
+            "set playerStateText to \"stopped\"",
+            "-e",
+            "end if",
+            "-e",
+            "end tell",
+        ],
+        capture_output=True,
+        text=True,
+    ).stdout.rstrip()
 
 
 # Return [name, artist, album, year, duration, player position] of current track
 # Only works when Apple Music is currently running
 def music_info_as():
-    return subprocess.run(["osascript",
-                           "-e", "tell application \"Music\"",
-                           "-e", "get {name, artist, album, year, duration} of current track & {player position}",
-                           "-e", "end tell"], capture_output=True, text=True).stdout.rstrip().split(', ')
+    return (
+        subprocess.run(
+            [
+                "osascript",
+                "-e",
+                "tell application \"Music\"",
+                "-e",
+                "get {name, artist, album, year, duration} of current track & {player position}",
+                "-e",
+                "end tell",
+            ],
+            capture_output=True,
+            text=True,
+        )
+        .stdout.rstrip()
+        .split(', ')
+    )
 
 
 # Return 'playing', 'paused' or 'stopped' at each call whenever Apple Music is running or not
@@ -66,17 +105,22 @@ while True:
                     large_image=APPICON,
                     large_text=APPICON_DESC,
                     small_image=state,
-                    small_text=f'{state.capitalize()}『{infos[0]}』by {infos[1]} ({infos[3]})',
+                    small_text=f'{state.capitalize()}『{infos[0]}』by {infos[1]}'
+                    + (f' ({infos[3]})' if infos[3] != '0' else ''),
                     details=infos[0],
-                    state=f'{infos[1]} — {infos[2]} ({infos[3]})',
-                    end=time.time() + float(infos[4]) - float(infos[5]))
+                    state=f'{infos[1]} — {infos[2]}'
+                    + (f' ({infos[3]})' if infos[3] != '0' else ''),
+                    end=time.time() + float(infos[4]) - float(infos[5]),
+                )
             except IndexError:
-                RPC.update(large_image=APPICON,
-                           large_text=APPICON_DESC,
-                           small_image=state,
-                           small_text=state.capitalize(),
-                           details=f'Apple Music is {state}',
-                           state='Details unavailable')
+                RPC.update(
+                    large_image=APPICON,
+                    large_text=APPICON_DESC,
+                    small_image=state,
+                    small_text=state.capitalize(),
+                    details=f'Apple Music is {state}',
+                    state='Details unavailable',
+                )
             time.sleep(15)
             state = music_state()
 
