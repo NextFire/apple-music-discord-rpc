b3c10c3d5c0d42b5eee5493d389463e5c673df01
ririxidev <mail@ririxi.dev>
2021-12-31 18:39:35 +0100

    style: Format config

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 import debug from "debug";
 
-export const APP_ID: string = '773825528921849856',
-  DEFAULT_APP_ICON: string = 'appicon',
-  logger = debug('am-discord-rpc');
+export const APP_ID: string = "773825528921849856",
+	DEFAULT_APP_ICON: string = "appicon",
+	logger = debug("am-discord-rpc");
